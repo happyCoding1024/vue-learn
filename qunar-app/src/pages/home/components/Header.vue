c7e615746596c4002e45cf27803b2c1e54e5bc7f
happyCoding1024 <zhangguicheng_1995@163.com>
2019-12-10 22:38:49 +0800

    add stylus

---
+++ qunar-app/src/pages/home/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-input">内容</div>
+    <div class="header-right">城市</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+};
+</script>
+
+<style lang="stylus" scoped>
+  .header {
+    display: flex
+    background: #00bcd4
+    line-height: .86em;
+    color: #fff
+  }
+
+    .header-left
+      width: .64rem
+      float: left
+    .header-input
+      flex: 1
+    .head-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      color: red
+</style>
