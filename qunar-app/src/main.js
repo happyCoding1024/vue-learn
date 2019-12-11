64a84874a002f5cd8d5a1cbb102f8a8e7150cdbc
happyCoding1024 <zhangguicheng_1995@163.com>
2019-12-11 15:21:25 +0800

    首页轮播图完成

--- qunar-app/src/main.js
+++ qunar-app/src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue';
 import fastClick from 'fastclick';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
 import 'styles/reset.css';
 import 'styles/border.css';
 import 'styles/iconfont.css';
 import App from './App';
 import router from './router';
 
+require('swiper/css/swiper.css');
+
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
+Vue.use(VueAwesomeSwiper);
 
 /* eslint-disable no-new */
 new Vue({
