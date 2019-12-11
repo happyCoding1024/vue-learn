ed9433caadd318a59346e4d4199769ce77cae515
happyCoding1024 <zhangguicheng_1995@163.com>
2019-12-11 10:56:56 +0800

    首页开发完成

--- qunar-app/src/pages/home/components/Header.vue
+++ qunar-app/src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">内容</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont" arrow-icon>&#xe62d;</span>
+    </div>
   </div>
 </template>
 
@@ -13,19 +21,33 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/varibles.styl';
   .header
     display: flex
-    background: #00bcd4
-    line-height: .86em;
+    line-height: .86rem
+    background: $bgcolor
     color: #fff
     .header-left
       width: .64rem
       float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
     .header-input
       flex: 1
-    .head-right
+      height: .64rem
+      line-height: .64rem
+      margin-left: .2rem
+      margin-top: .12rem
+      background: #fff
+      padding-left: .2rem
+      border-radius: .1rem
+      color: #ccc
+    .header-right
       width: 1.24rem
       float: right
       text-align: center
-      color: red
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
 </style>
