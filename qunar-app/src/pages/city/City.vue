9498a509db62aa8be416eaebea55854213fdf62c
happyCoding1024 <zhangguicheng_1995@163.com>
2019-12-12 17:10:44 +0800

    city router config

---
+++ qunar-app/src/pages/city/City.vue
@@ -0,0 +1,18 @@
+<template>
+  <city-header></city-header>
+</template>
+
+<script>
+import CityHeader from './components/Header';
+
+export default {
+  name: 'city',
+  components: {
+    CityHeader,
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
