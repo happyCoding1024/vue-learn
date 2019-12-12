30ddef2723493f24b7ac23d5a3a0cfbe3cad2b7d
happyCoding1024 <zhangguicheng_1995@163.com>
2019-12-12 08:18:44 +0800

    首页推荐页面完成

--- qunar-app/src/pages/home/components/Icons.vue
+++ qunar-app/src/pages/home/components/Icons.vue
@@ -83,35 +83,37 @@ export default {
   .icons >>> .swiper-container
     height: 0
     padding-bottom: 50%
-  .icon
-    height: 0
-    overflow: hidden
-    position: relative
-    float: left
-    width: 25%
-    padding-bottom: 25%
-    .icon-img
-      position: absolute
-      top: 0
-      left: 0
-      right: 0
-      bottom: .44rem
-      border-sizing: border-box
-      padding: .1rem
-      .icon-img-content
-        display: block
-        margin: 0 auto
-        height: 100%
-    .icon-desc
-      position: absolute
-      bottom: 0
-      left: 0
-      right: 0
-      height: .44rem
-      line-height: .44rem
-      text-align: center
-      color: $darkTextColor
+  .icons
+    margin-top: .1rem
+    .icon
+      height: 0
       overflow: hidden
-      white-space: nowrap
-      ellipsis()
+      position: relative
+      float: left
+      width: 25%
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        border-sizing: border-box
+        padding: .1rem
+        .icon-img-content
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        bottom: 0
+        left: 0
+        right: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+        overflow: hidden
+        white-space: nowrap
+        ellipsis()
 </style>
