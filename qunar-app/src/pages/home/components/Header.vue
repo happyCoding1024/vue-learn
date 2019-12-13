3db57f04a5393a7374136853dc60c794fcf323a0
happyCoding1024 <zhangguicheng_1995@163.com>
2019-12-13 11:02:52 +0800

    search completed

--- qunar-app/src/pages/home/components/Header.vue
+++ qunar-app/src/pages/home/components/Header.vue
@@ -2,9 +2,9 @@
   <div class="header">
     <div class="header-left">
       <div class="iconfont back-icon">&#xe624;</div>
-    </div>
-    <div class="header-input">
-      <span class="iconfont">&#xe632;</span>
+  </div>
+  <div class="header-input">
+    <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
     <router-link to="/city">
